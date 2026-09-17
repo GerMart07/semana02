7cff8572491ee9849224d30844adc1704052bba3
Luis Gerardo Martinez <luis.g.martinez.238107@gmail.com>
2026-09-16 22:08:50 -0600

    Commit 2: Trabajando con variables

--- variables.py
+++ variables.py
@@ -1,4 +1,9 @@
 # Esta sesión sera para aprender el uso de variables
-print("hola mundo")
-print("hoy es miércoles")
-print("mañana es jueves")
+print("Sistema de mensajería")
+distancia_km = 9.7
+peso_kg = 2.0 
+es_urgente = True
+zona = "B"
+
+distancia_km = distancia_km + 16
+print(distancia_km)
